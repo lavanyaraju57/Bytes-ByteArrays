40cc4e8610535d2350e57c638581ac3047a3117a
lavanyaraju57 <99262310+lavanyaraju57@users.noreply.github.com>
2022-03-08 11:44:41 +0530

    Add files via upload

---
+++ Bytes & ByteArray.py
@@ -0,0 +1,29 @@
+# y=b"python"
+# print(y,type(y))
+# print(list(y))
+# print(tuple(y))
+# print(set(y))
+
+# y=b"python"
+# for i in y:
+#     print(i)
+
+# y=b"python"
+# y[0]=80
+# print(y[0])
+# print(y[:3])
+# print(y[::-1])
+
+# v=b'[1,2,3]'
+# print(list(v))
+
+# m=bytearray(b"python")
+# print(m)
+# print(m[0])
+# m[0]=80
+# print(m)
+# print(m[:])
+# print(m[:3])
+# print(m[::-1])
+
+
